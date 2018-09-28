20df06430f6cdf7d812f27238b44578c7dc1da8c
Mateusz Leśko <31209474+mateuszlesko@users.noreply.github.com>
2018-09-28 14:58:29 +0200

    Developed update
    Now, inputs are showing a data from db.
    Adding  security hole occurs -> htmlspecialchars()

---
+++ updateShop.html.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="../Eureka/styles/questions.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<form action='updateShop.php' method='POST'>
+    <input type='hidden' name='ID' value = "<?php echo $_GET['ShopID'];?>">
+            <div class='form-group'>
+        <label>Miasto</label><br>
+        <input type='text' class="form-control"name='M'required="true" value="<?php echo $_GET['ShopTown']?>"><br>
+        </div>
+        <div class='form-group'>
+        <label>Ulica</label><br>
+        <input type='text' class="form-control"name='U'required="true" value="<?php echo $_GET['ShopStreet']?>"><br>
+        </div>
+        <div class='form-group'>
+        <label>Kontakt</label><br>
+        <input type='text' class="form-control"name='U'required="true" value="<?php echo $_GET['ShopContact']?>"><br>
+        </div>
+        <input type='submit' name='S' class="btn btn-primary" value='Wyslij'><br>
+        
+
+    
+    
+</form>
+</div>
+</body>
+</html>
---
+++ updateShop.php
@@ -0,0 +1,19 @@
+<?php
+function updateShop($pdo,$id){
+try{
+    include 'connection.php';
+    $query = $pdo->prepare("UPDATE sklep set  Miasto=:Miasto,Ulica=:Ulica,Kontakt:Kontakt where ID=:id");
+    $query->execute(array('id'=>$id,
+    'Miasto'=>$_POST['M'],
+    'Ulica'=>$_POST['U'],
+    'Kontakt'=>$_POST['K']));
+
+header('location:shop.php');
+}
+
+catch(PDOException $e){
+    echo 'error '.$e;
+}
+}
+updateShop($pdo,$_POST['ID']);
+?>
