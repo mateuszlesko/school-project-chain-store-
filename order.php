4cb743afd45e1bb1760ee2d3e9fcc2b5981ac115
Mateusz Leśko <31209474+mateuszlesko@users.noreply.github.com>
2018-10-29 19:10:05 +0100

    Fixed error in orders
    now it works fine.

--- order.php
+++ order.php
@@ -5,15 +5,13 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <?php
-$price = 0;
-$nameOfOrder = '';
-$name = $_POST['M']; 
-$surname = $_POST['N'];
-$email = $_POST['E'];
-//dokoncz formularz i skrypt php
-function getOrder($o){
 
-$date = date('d.M.Y h:i');
+$nameOfOrder = '';
+
+//dokoncz formularz i skrypt php
+function getPriceOrder($o){
+
+$price=0;
     switch($o){
         case 1:
         $price = 100;
@@ -38,11 +36,45 @@ $date = date('d.M.Y h:i');
         break;
 
     }
-    include 'connection.php';
-    $sql = 
+    return $price;
+    
   
 }
+function getNameOrder($o){
+  $name = "";
+  switch($o){
+    case 1:
+    
+    $name = 'zlozenie komputera';
+    break;
+    case 2:
+   
+    $name = 'stworzenie lokalnej sieci komputerowej';
+    break;
+    case 3:
+    
+    $name = 'hosting';
+    break;
+    case 4:
+   
+    $name = 'stworzenie bazy danych';
+    break;
+
+    default:
+    
+    $name = '';
+    break;
+  }
+  return $name;
+}
 function seeOrder(){
+  $name = $_POST['M']; 
+$surname = $_POST['N'];
+$email = $_POST['E'];
+$date = date('d.M.Y h:i');
+$price = getPriceOrder($_POST['W']);
+$nameOfOrder = getNameOrder($_POST['W']);
+
   echo "<ul class='list-group'>
   <li class='list-group-item active'>Zamowienie: $name $surname </li>
   <li class='list-group-item'>Zlecona usluga: $nameOfOrder</li>
@@ -52,7 +84,6 @@ function seeOrder(){
   
 </ul>";
 }
-getOrder($_POST['W']);
 seeOrder();
 
 ?>
